1640e9499ff3b6b429d2a6909202933698dec0c6
sung-yeop <jjabc9459@naver.com>
2025-03-30 12:43:14 +0900

    test: 설정파일 수정 및 로깅 추가 & 파이프라인 테스트

--- src/main/java/org/example/apigateway/ApigatewayApplication.java
+++ src/main/java/org/example/apigateway/ApigatewayApplication.java
@@ -1,15 +1,26 @@
 package org.example.apigateway;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@Slf4j
 public class ApigatewayApplication {
+
+    @Value("${eureka.client.service-url.defaultZone}")
+    private String url;
 
     public static void main(String[] args) {
         SpringApplication.run(ApigatewayApplication.class, args);
+    }
+
+    @jakarta.annotation.PostConstruct
+    public void init() {
+        log.info("---------------EUREKA URL ------------ {}", url);
     }
 
 }
